769073f901c8667658f37d7706e6235db3074c76
victor291201 <victor291201hugo@gmail.com>
2022-03-08 22:55:08 -0500

    produccion v3

--- src/conexion.js
+++ src/conexion.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
 
@@ -20,27 +19,4 @@ conexion.connect(function(error){
     }
 });
 
-=======
-const express = require("express");
-const app = express();
-
-var mysql = require("mysql");
-const Connection = require("mysql/lib/Connection");
-
-var conexion = mysql.createConnection({
-    host:"localhost",
-    database:"notas_movil",
-    user:"root",
-    password:"291201"
-});
-
-conexion.connect(function(error){
-    if(error){
-        throw error;
-    }else{
-        console.log("base de datos conectada")
-    }
-});
-
->>>>>>> 3ca02ed078cb355ec459bf25155afe512a9b88f8
 module.exports = conexion;
